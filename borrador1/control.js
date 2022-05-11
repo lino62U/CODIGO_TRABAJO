30ed3707b3f42534312070342ece650eafd3c843
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-05-11 17:39:24 -0500

    Update control.js

--- borrador1/control.js
+++ borrador1/control.js
@@ -1,51 +1,86 @@
-console.clear();
+const lista = ["APAZA APAZA NELZON JORGE","APAZA QUISPE ANGEL ABRAHAM","BENAVENTE AGUIRRE PAOLO DANIEL",
+"CACSIRE SANCHEZ JHOSEP ANGEL","CARAZAS QUISPE ALESSANDER JESUS","CASTILLO SANCHO SERGIO","CAYLLAHUA GUTIERREZ DIEGO YAMPIER",
+"CCAMA MARRON GUSTAVO ALONSO","CERPA GARCIA JEAN FRANCO","CONDORI CASQUINO EBERT LUIS","DAVIS COROPUNA LEON FELIPE",
+"ESCARZA PACORI ALEXANDER RAUL","GONZALES CONDORI ALEJANDRO JAVIER","GUTIERREZ ZEVALLOS JAIME JOSÉ","HUALPA LOPEZ JOSE MAURICIO",
+"HUAMAN COAQUIRA LUCIANA JULISSA","LAZO PAXI NATALIE MARLENY","LOPEZ CONDORI ANDREA DEL ROSARIO","LUPO CONDORI AVELINO",
+"MALDONADO CASILLA BRAULIO NAYAP","MALDONADO P ROY ABEL","MARIÑOS HILARIO PRINCCE YORWIN","MARTÍNEZ CHOQUE ALDO RAÚL",
+"MAYORGA VILLENA JHAROLD ALONSO","MENA QUISPE SERGIO SEBASTIAN SANTOS","MOGOLLON CACERES SERGIO DANIEL","MONTOYA CHOQUE LEONARDO",
+"NIZAMA CESPEDES JUAN CARLOS ANTONIO","OLAZÁBAL CHÁVEZ NEILL ELVERTH","PARDAVÉ ESPINOZA CHRISTIAN","PARIZACA MOZO PAUL ANTONY",
+"QUILCA HUAMANI BRYAN","QUISPE ROJAS JAVIER WILBER","ROQUE SOSA OWEN HAZIEL","RUIZ MAMANI EDUARDO GERMAN","SUCASACA CHIRE EDWARD HENRY",
+"TAYA YANA SAMUEL OMAR","YAVAR GUILLEN ROBERTO GUSTAVO","ZAMALLOA MOLINA SEBASTIAN AGENOR","ZHONG CALLASI LINGHAI JOAQUIN"];
 
-var id=0;
+var matriz = [];
 
-var getId=()=>++id;
-const isEmpty= (str) =>str.trim()==='';
-
-function agregar(){
-    var nombre = document.getElementById('nombre');
-    var apellido = document.getElementById('apellido');
-    var email = document.getElementById('email');
-    var telefono = document.getElementById('telefono');
-
-    nombreVal = nombre.value;
-    apellidoVal = apellido.value;
-    emailVal = email.value;
-    telefonoVal = telefono.value;
-    console.log(telefono);
-
-    tablaUsuarios = document.getElementById('tablaUsuarios');
-
-    if(isEmpty(nombreVal)|| isEmpty(apellidoVal)|| isEmpty(emailVal) || isEmpty(telefonoVal)){
-        alert('Datos obligatorios');
-        return;
-    }
-    let id = getId();
-    
-    let fila =  '<tr id="id"><td>'+id+'</td><td>'+ nombreVal + '</td><td>'+ apellidoVal + '</td><td>' +emailVal+ '</td><td>' +telefonoVal+ '</td>  <td> <button class="btnEliminar">Eliminar </button></td></tr>';
-    
-    tablaUsuarios.lastElementChild.insertAdjacentHTML('beforeend',fila);
-
-    nombre.value=' ';
-    apellidoVal.value=' ';
-    emailVal.value=' ';
-    telefono.value=' ';
-    nombre.focus();    
+matriz = new Array(40);
+for(let i=0;i<lista.length;i++){
+    matriz[i] = new Array(4);
 }
 
-document.addEventListener('click',(e) =>{
-    if(e.target.classList.contains('btnEliminar')){
-    e.target.parentElement.parentElement.remove();
+for(let i=0;i<lista.length;i++){
+    matriz[i][0] = i+1;
+    matriz[i][1] = lista[i];
+    matriz[i][2];
+    matriz[i][3] = "";
+}
+
+for(let i=0;i<lista.length;i++){
+    const tabla_inicio = document.getElementById('inicio_cuerpo');
+    tabla_inicio.insertAdjacentHTML('beforeend', `<tr></tr>`);
+    let jus= `<td></td>`;
+    
+    let indice;
+    if(i>=0 && i<9){
+        indice=`0${i+1}`;
+    }else{
+        indice=i+1;
     }
+
+    let contenido = `   <td class="btsLISA">${indice}</td>
+                        <td>${lista[i]}</td>
+                        <td id="${i}">
+                            <input type="submit" value="P" id="${1 + (i*3)}">
+                            <input type="submit" value="T" id="${2 + (i*3)}">
+                            <input type="submit" value="F" id="${3 + (i*3)}">
+                        </td>`
+    tabla_inicio.lastElementChild.insertAdjacentHTML('beforeend',contenido);
+    tabla_inicio.lastElementChild.insertAdjacentHTML('beforeend',jus);
+}
+
+function crearJusti(dni,estado){
+    let texto = `<td><input type="text"></td>`
+    let jus =`<td></td>`
+    const padre = document.getElementById(dni);
+    const hijos = padre.parentElement.parentElement.childElementCount;
+    if((estado == "T" || estado == "F") && hijos < 5){
+        padre.parentElement.parentElement.lastElementChild.remove();
+        padre.parentElement.parentElement.lastElementChild.insertAdjacentHTML('afterend',texto);
+    }
+    if(estado == "P" && hijos > 3){
+        padre.parentElement.parentElement.lastElementChild.remove();
+        padre.parentElement.parentElement.lastElementChild.insertAdjacentHTML('afterend',jus);
+    }
+}
+
+document.addEventListener('click' , (e) =>{
+
+    const boton = e.target;
+    getID = parseInt(boton.id);
+
+    if(getID % 3 == 0){
+        console.log("FALTA");
+        matriz[parseInt(boton.parentElement.id)][2] = "F";
+    }
+    if(getID % 3 == 1){
+        console.log("PRESENTE");
+        matriz[parseInt(boton.parentElement.id)][2] = "P";
+
+    }
+    if(getID % 3 == 2){
+        console.log("TARDANZA");
+        matriz[parseInt(boton.parentElement.id)][2] = "T";
+    }
+
+    console.log(matriz[parseInt(boton.parentElement.id)]);
+
+    crearJusti(getID, matriz[parseInt(boton.parentElement.id)][2])
 })
-function asignar(){
-    btnAgregar = document.getElementById('btnAgregar');
-    console.log("HOLA MUNDO");
-    btnAgregar.addEventListener('click',agregar);
-}
-
-window.addEventListener('load', asignar);
-
