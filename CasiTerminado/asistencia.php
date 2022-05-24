7499cbe06a05312e84c1e65a7c1c0a97fcfb6c56
LeonDavisCoropuna <104030173+LeonDavisCoropuna@users.noreply.github.com>
2022-05-24 00:11:19 -0500

    Update asistencia.php

--- CasiTerminado/asistencia.php
+++ CasiTerminado/asistencia.php
@@ -1,20 +1,12 @@
 <?php
     $datos = $_POST;
 
-//////////////////////////
     $conexion = new mysqli("localhost","root","","asistencia");
 
     $fecha = $_POST['archivo'];
-
-    if($conexion->connect_error){
-        echo "ZZZZ";
-    }
-    else{
-        echo "NICE";
-    }
     
-    $sql = "CREATE TABLE $fecha(
-        N INT(100) NOT NULL,
+    $sql = "CREATE TABLE `$fecha`(
+        N INT(5) NOT NULL,
         NOMBRE VARCHAR(100) NOT NULL,
         ASISTENCIA VARCHAR(20) NOT NULL,
         JUSTIFICACION VARCHAR(200) NOT NULL
@@ -22,31 +14,16 @@
 
     $conexion->query($sql);
     
-////////////////////
     $conex = mysqli_connect("localhost","root","","asistencia");
 
-    for($j=0;$j<40;$j++){
+    for($j=1;$j<41;$j++){
         $orden = $j;
-        $name = $datos["nombre".$j];
-        $estado = $datos["estado".$j];
-        $justi = $datos["justi".$j];
-        $consulta = "INSERT INTO $fecha(N,NOMBRE,ASISTENCIA,JUSTIFICACION) VALUES ('$orden','$name','$estado','$justi')";
+        $name = $datos["nombre".($j-1)];
+        $estado = $datos["estado".($j-1)];
+        $justi = $datos["justi".($j-1)];
+        $consulta = "INSERT INTO `$fecha`(N,NOMBRE,ASISTENCIA,JUSTIFICACION) VALUES ('$orden','$name','$estado','$justi')";
         mysqli_query($conex,$consulta);
     }
 
     echo $datos['nombre10'];
-
-/*
-    
-    $sql = "CREATE DATABASE asistencia";
-
-    if($conexion->query($sql) === true){
-        echo "base de datos creada";    
-    }
-    else{
-        die("NO SE CREO LA DB ".$conexion_error);
-    }
-    
-*/
-
 ?>
