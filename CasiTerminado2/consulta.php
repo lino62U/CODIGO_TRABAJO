b3cfc029c398779853a3a553528d1cc3eb1d1fff
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2022-05-25 19:07:54 -0500

    Create consulta.php

---
+++ CasiTerminado2/consulta.php
@@ -0,0 +1,50 @@
+<?php
+    $leer = $_GET['base'];
+?>
+<?php
+ob_start();
+$conex = mysqli_connect("localhost","root","","asistencia");
+
+    $consulta = "SELECT * FROM  `$leer`";
+    $resultado = mysqli_query($conex,$consulta);
+    if($resultado){
+        echo "<table>";
+        echo "<thead";
+        echo "<tr>";
+        echo "<td>N</td>";
+        echo "<td>NOMBRES Y APELLIDOS</td>";
+        echo "<td>ESTADO</td>";
+        echo "<td>JUSTIFICACIÓN</td>";
+        echo "</thead";
+
+        while($row = $resultado->fetch_array()){    
+
+            $id = $row['N'];
+            $nombre = $row["NOMBRE"];
+            $estado = $row["ASISTENCIA"];
+            $justi = $row["JUSTIFICACION"];
+            
+            echo "<tr>";
+                echo "<td>$id</td>";
+                echo "<td>$nombre</td>";
+                echo "<td>$estado</td>";
+                echo "<td>$justi</td>";
+
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+
+    $html = ob_get_clean();
+
+    require_once 'dompdf/autoload.inc.php';
+
+    use Dompdf\Dompdf;
+    $dompdf = new Dompdf();
+
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('letter');
+    $dompdf->render();
+    $dompdf->stream("{$leer}.pdf",array("Attachment" => true));
+    echo "<tr><td>ff</td></tr>";
+?>
